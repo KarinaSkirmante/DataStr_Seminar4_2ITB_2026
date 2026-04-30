ad0cfce51358d8a332eb1c50d137319ef75626d7
KarinaSkirmante <karina.krinkele@gmail.com>
2026-04-30 11:50:44 +0300

    Edit: added add and addhelper(not finished) in the mYBST

--- src/datastr/MyBST.java
+++ src/datastr/MyBST.java
@@ -24,4 +24,28 @@ public class MyBST<Ttype> {
 			return true;
 		}
 	}
+	
+	
+	public void add(Ttype element) throws Exception {
+		if(isFull()) {
+			throw new Exception("Koks ir pilns un nevar vairs pievienot jaunus elementus");
+		}
+		
+		addHelper(rootNode, element);
+		howManyElements++;
+	}
+	
+	private void addHelper(MyNode<Ttype> nodeTemp, Ttype element) {
+		if(nodeTemp != null) {
+			if(((Comparable)element).compareTo(nodeTemp.getElement()) > 0) {
+				addHelper(nodeTemp.getRightChNode(), element);
+			}
+			else
+			{
+				addHelper(nodeTemp.getLeftChNode(), element);
+			}
+			//TODO pabeigt
+			
+		}
+	}
 }
